f99862ff4f48dbcdb11abeee6ae22c35eecb15a3
Luminto <luminto_luhur@hotmail.com>
2016-04-28 18:21:26 +0700

    UDP and TCP connections

--- Sister/src/Server/TCPServer.java
+++ Sister/src/Server/TCPServer.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Testing;
-
 import java.io.*; 
 import java.net.*; 
 
-class TCPServer {    
+public class TCPServer {    
 	public static void main(String argv[]) throws Exception {          
 		String clientSentence;          
 		String capitalizedSentence;          
